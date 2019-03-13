0777d5fbda61cfd631a4a742ba6ce78ed92ad837
pkrueger <philip.krueger@caci.com>
2019-03-13 10:31:07 -0700

    first push

--- Rectangles/Program.cs
+++ Rectangles/Program.cs
@@ -3,19 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ 
+   create all recangles, store them in an array.
+   loop over rectangles, computing area and perimeter
+   and store those in respective arrays. Sort the
+   perimiter and area arrays and print.
+   
+   */
 namespace Rectangles
 {
   class Program
   {
     static void Main(string[] args)
     {
-      // The code provided will print ‘Hello World’ to the console.
-      // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-      Console.WriteLine("Hello World!");
-      Console.ReadKey();
+      int NUM_OF_RECTS = 5;
+      RectEZ[] rectangles = {
+        new RectEZ(2, 3),
+        new RectEZ(4, 5),
+        new RectEZ(3, 1),
+        new RectEZ(6, 2),
+        new RectEZ(3, 5)
+      };
 
-      // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+      double[] perimeters = new double[NUM_OF_RECTS];
+      double[] areas = new double[NUM_OF_RECTS];
+      for (int i = 0; i < NUM_OF_RECTS; i++)
+      {
+        perimeters[i] = rectangles[i].perimeter();
+        areas[i] = rectangles[i].area();
+        rectangles[i].print();
+      }
+      Array.Sort(perimeters);
+      Array.Sort(areas);
+      Console.Write("Perimeters Ascending: ");
+      foreach (var perimiter in perimeters)
+      {
+        Console.Write(perimiter + ", ");
+      }
+      Console.WriteLine();
+      Console.Write("Areas Ascending: ");
+      foreach (var area in areas)
+      {
+        Console.Write(area + ", ");
+      }
+      Console.ReadKey();
     }
   }
 }
---
+++ Rectangles/RectEZ.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Rectangles
+{
+  class RectEZ
+  {
+    private double length = 1;
+    private double width = 1;
+
+    public RectEZ(double length, double width)
+    {
+      if (length > 0 && width > 0)
+      {
+        this.length = Math.Abs(length);
+        this.width = Math.Abs(width);
+      }
+    }
+
+    public void setWidth(double width)
+    {
+      if (width > 0)
+        this.width = Math.Abs(width);
+    }
+
+    public void setLength(double length)
+    {
+      if (length > 0)
+        this.length = Math.Abs(length);
+    }
+
+    public double area()
+    {
+      return length * width;
+    }
+
+    public double perimeter()
+    {
+      return 2 * length + 2 * width;
+    }
+
+    public void print()
+    {
+      Console.WriteLine("Rectangle: Length: " + length + ", Width: " + width + ", Area: " + area() + ", Perimeter: " + perimeter());
+    }
+  }
+}
